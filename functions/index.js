9dded627c98085aad7d92cb651bcdbefb9dfad3e
Cassandra Curcio <cassandra.curcio@gmail.com>
2022-08-12 20:54:54 -0400

    api to connect to firestore db for message webapp

---
+++ functions/index.js
@@ -0,0 +1,13 @@
+import functions from "firebase-functions";
+import express from "express";
+import cors from "cors";
+import { getMessage, createMessage } from "./functions.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/messages", getMessage);
+app.post("/add-message", createMessage);
+
+export const api = functions.https.onRequest(app);
